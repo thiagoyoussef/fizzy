ca0d61477d83d7a8811892280cc44f1065e58c97
Kevin McConnell <kevin@37signals.com>
2025-08-20 16:37:09 +0100

    Add `if_exists` when dropping `commands`
    To cover anyone who may have run the original migration before it was
    removed.

--- db/migrate/20250819105245_drop_commands_table.rb
+++ db/migrate/20250819105245_drop_commands_table.rb
@@ -1,5 +1,5 @@
 class DropCommandsTable < ActiveRecord::Migration[8.1]
   def change
-    drop_table :commands
+    drop_table :commands, if_exists: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_08_18_104141) do
+ActiveRecord::Schema[8.1].define(version: 2025_08_19_105245) do
   create_table "accesses", force: :cascade do |t|
     t.datetime "accessed_at"
     t.integer "collection_id", null: false
