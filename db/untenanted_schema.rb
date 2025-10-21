6aafa3aff126bfd310f534ba2bcff7e6341d11fe
Mike Dalessio <mike@37signals.com>
2025-10-20 17:47:42 -0700

    Update migration versions to 8.2

--- db/untenanted_schema.rb
+++ db/untenanted_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_10_07_112917) do
+ActiveRecord::Schema[8.2].define(version: 2025_10_07_112917) do
   create_table "identities", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
