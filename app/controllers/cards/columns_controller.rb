c31354d6c64ed95dcd01330145fae359edf4f340
Jorge Manrubia <jorge@hey.com>
2025-11-06 14:31:19 +0100

    Fix: show columns in the right order
    https://app.fizzy.do/5986089/cards/2820

--- app/controllers/cards/columns_controller.rb
+++ app/controllers/cards/columns_controller.rb
@@ -1,7 +1,7 @@
 class Cards::ColumnsController < ApplicationController
   def edit
     @card = Current.user.accessible_cards.find(params[:card_id])
-    @columns = @card.board.columns
+    @columns = @card.board.columns.sorted
 
     fresh_when etag: [ @card, @columns ]
   end
