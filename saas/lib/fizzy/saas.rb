5e3c9c1d9274710e98ddff29e4d285092cf88db7
Jorge Manrubia <jorge@hey.com>
2025-11-25 13:05:06 +0100

    Add method to invoke from the rails runner

--- saas/lib/fizzy/saas.rb
+++ saas/lib/fizzy/saas.rb
@@ -3,5 +3,10 @@ require "fizzy/saas/engine"
 
 module Fizzy
   module Saas
+    def self.append_test_paths
+      saas_test_dir = "#{Gem::Specification.find_by_name("fizzy-saas").gem_dir}/test"
+      ENV["DEFAULT_TEST"] = "{#{saas_test_dir},test}/**/*_test.rb"
+      ENV["DEFAULT_TEST_EXCLUDE"] = "{#{saas_test_dir},test}/{system,dummy,fixtures}/**/*_test.rb"
+    end
   end
 end
